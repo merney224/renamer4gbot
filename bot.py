13a816c45ba25b63c2bf83d08159df8026caa720
merney224 <merney224@gmail.com>
2025-09-18 14:00:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import os
 
 from plugins.cb_data import app as Client2
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "8152348968:AAGo_GiGkefoYH3pILeAN8T6aySP2nV4Oxk")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "19481274"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "edbb4d162c4871c25ccd6f7179dad8cf")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQCEW4EeuFlxOgnqbBx6xIZDCjhEI8WNVt29Z6AtLL-OozpUF6et-QEt1Ujrj3z8xBgPYqfdoNana1V5BXr9n1RRp6GRVygCNcHCeMfaMOguce1GT3sOFjsUehlIy4QSzLed3_zRC-zy6mgXBQDARi2ZHT6xoQS9J6nRsWEKoIOBHaFaQCM316Wfp2o4LkyGjs3JgLtYrUUA-w6daP6WE9bMRequzkNYXkvfh1nZZIK1sgg95evbhvbqe7gHlraKWR6fMZjxSAFEEJ8zhugE7wGHtC_qugGgZ6Hy4-ilbDz5dzt9s-AiPmTfJ4GKb0jQkXtYn8qZovmAaiakgTYV67miAAAAAYiHvBkA")
 
 
 
@@ -37,3 +37,4 @@ if STRING:
     
 else:
     bot.run()
+
